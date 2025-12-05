efb45674681ac5a5306648535b58b6d706b39d41
akrishnaams <arunkrishna.annaimadalamsivasubramanian@epfl.ch>
2025-12-05 18:03:47 +0000

    PageRank Workload Makefile and Dockerfile creation

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM akrishnaams/ubuntu2204:oh-my-bash
+# FROM ubuntu:22.04
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update -qq && \
+    apt-get install -y -qq make g++
+
+WORKDIR /root/gapbs
+
+RUN mkdir -p /root/gapbs/
+
+COPY ./src /root/gapbs/src
+COPY ./pagerank.mk /root/gapbs/Makefile
+
+RUN make all
+
+
+
---
+++ pagerank.mk
@@ -0,0 +1,173 @@
+# Makefile-based Benchmarking Infrastructure
+# Scott Beamer, 2017
+
+
+
+# Generate Input Graphs ------------------------------------------------#
+#-----------------------------------------------------------------------#
+
+GRAPH_DIR = benchmark/graphs
+RAW_GRAPH_DIR = benchmark/graphs/raw
+
+GRAPHS = twitter web road kron urand
+ALL_GRAPHS =\
+	$(addsuffix .sg, $(GRAPHS)) \
+	$(addsuffix U.sg, $(GRAPHS))
+ALL_GRAPHS_WITH_PATHS = $(addprefix $(GRAPH_DIR)/, $(ALL_GRAPHS))
+
+$(RAW_GRAPH_DIR):
+	mkdir -p $@
+
+.PHONY: bench-graphs
+bench-graphs: $(RAW_GRAPH_DIR) $(ALL_GRAPHS_WITH_PATHS)
+
+
+# Real-world
+
+TWITTER_URL = https://github.com/ANLAB-KAIST/traces/releases/download/twitter_rv.net/twitter_rv.net.$*.gz
+$(RAW_GRAPH_DIR)/twitter_rv.net.%.gz:
+	wget -P $(RAW_GRAPH_DIR) $(TWITTER_URL)
+
+$(RAW_GRAPH_DIR)/twitter_rv.net: $(RAW_GRAPH_DIR)/twitter_rv.net.00.gz $(RAW_GRAPH_DIR)/twitter_rv.net.01.gz $(RAW_GRAPH_DIR)/twitter_rv.net.02.gz $(RAW_GRAPH_DIR)/twitter_rv.net.03.gz
+	gunzip -c $^ > $@
+	touch $@
+
+$(RAW_GRAPH_DIR)/twitter.el: $(RAW_GRAPH_DIR)/twitter_rv.net
+	rm -f $@
+	ln -s twitter_rv.net $@
+
+$(GRAPH_DIR)/twitter.sg: $(RAW_GRAPH_DIR)/twitter.el converter
+	./converter -f $< -b $@
+
+$(GRAPH_DIR)/twitterU.sg: $(RAW_GRAPH_DIR)/twitter.el converter
+	./converter -sf $< -b $@
+
+ROAD_URL = http://www.dis.uniroma1.it/challenge9/data/USA-road-d/USA-road-d.USA.gr.gz
+$(RAW_GRAPH_DIR)/USA-road-d.USA.gr.gz:
+	wget -P $(RAW_GRAPH_DIR) $(ROAD_URL)
+
+$(RAW_GRAPH_DIR)/USA-road-d.USA.gr: $(RAW_GRAPH_DIR)/USA-road-d.USA.gr.gz
+	cd $(RAW_GRAPH_DIR)
+	gunzip < $< > $@
+
+$(GRAPH_DIR)/road.sg: $(RAW_GRAPH_DIR)/USA-road-d.USA.gr converter
+	./converter -f $< -b $@
+
+$(GRAPH_DIR)/roadU.sg: $(RAW_GRAPH_DIR)/USA-road-d.USA.gr converter
+	./converter -sf $< -b $@
+
+WEB_URL = https://sparse.tamu.edu/MM/LAW/sk-2005.tar.gz
+$(RAW_GRAPH_DIR)/sk-2005.tar.gz:
+	wget -P $(RAW_GRAPH_DIR) $(WEB_URL)
+
+$(RAW_GRAPH_DIR)/sk-2005/sk-2005.mtx: $(RAW_GRAPH_DIR)/sk-2005.tar.gz
+	tar -zxvf $< -C $(RAW_GRAPH_DIR)
+	touch $@
+
+$(GRAPH_DIR)/web.sg: $(RAW_GRAPH_DIR)/sk-2005/sk-2005.mtx converter
+	./converter -f $< -b $@
+
+$(GRAPH_DIR)/webU.sg: $(RAW_GRAPH_DIR)/sk-2005/sk-2005.mtx converter
+	./converter -sf $< -b $@
+
+
+# Synthetic
+
+KRON_ARGS = -g27 -k16
+$(GRAPH_DIR)/kron.sg: converter
+	./converter $(KRON_ARGS) -b $@
+
+$(GRAPH_DIR)/kronU.sg: $(GRAPH_DIR)/kron.sg converter
+	rm -f $@
+	ln -s kron.sg $@
+
+KRON_ARGS_28 = -g28 -k16
+$(GRAPH_DIR)/kron28.sg: converter
+	./converter $(KRON_ARGS_28) -b $@
+
+$(GRAPH_DIR)/kron28U.sg: $(GRAPH_DIR)/kron28.sg converter
+	rm -f $@
+	ln -s kron28.sg $@
+
+KRON_ARGS_29 = -g29 -k16
+$(GRAPH_DIR)/kron29.sg: converter
+	./converter $(KRON_ARGS_29) -b $@
+
+$(GRAPH_DIR)/kron29U.sg: $(GRAPH_DIR)/kron29.sg converter
+	rm -f $@
+	ln -s kron29.sg $@
+
+
+URAND_ARGS = -u27 -k16
+$(GRAPH_DIR)/urand.sg: converter
+	./converter $(URAND_ARGS) -b $@
+
+$(GRAPH_DIR)/urandU.sg: $(GRAPH_DIR)/urand.sg converter
+	rm -f $@
+	ln -s urand.sg $@
+
+
+
+# Benchmark Execution --------------------------------------------------#
+#-----------------------------------------------------------------------#
+
+OUTPUT_DIR = benchmark/out
+
+$(OUTPUT_DIR):
+	mkdir -p $@
+
+# Ordered to reuse input graphs to increase OS file cache hit probability
+BENCH_ORDER = pr-twitter pr-web pr-road pr-kron pr-urand
+
+OUTPUT_FILES = $(addsuffix .out, $(addprefix $(OUTPUT_DIR)/, $(BENCH_ORDER)))
+
+.PHONY: pagerank-all
+pagerank-all: BENCH_ORDER = pr-twitter pr-web pr-road pr-kron pr-urand
+pagerank-all: $(OUTPUT_DIR) $(OUTPUT_FILES)
+
+$(OUTPUT_DIR)/pr-%.out: $(GRAPH_DIR)/%.sg pr
+	./pr -f $< -i1000 -t1e-4 -n32 > $@
+
+.PHONY: pagerank-kron
+BENCH_ORDER = pr-kron
+pagerank-kron: $(OUTPUT_DIR) $(OUTPUT_FILES)
+
+
+.PHONY: pagerank-kron28
+BENCH_ORDER = pr-kron28
+pagerank-kron28: $(OUTPUT_DIR) $(OUTPUT_FILES)
+
+
+.PHONY: pagerank-kron29
+BENCH_ORDER = pr-kron29
+pagerank-kron29: $(OUTPUT_DIR) $(OUTPUT_FILES)
+
+
+CXX_FLAGS += -std=c++11 -O3 -Wall
+PAR_FLAG = -fopenmp
+
+ifneq (,$(findstring icpc,$(CXX)))
+	PAR_FLAG = -openmp
+endif
+
+ifneq (,$(findstring sunCC,$(CXX)))
+	CXX_FLAGS = -std=c++11 -xO3 -m64 -xtarget=native
+	PAR_FLAG = -xopenmp
+endif
+
+ifneq ($(SERIAL), 1)
+	CXX_FLAGS += $(PAR_FLAG)
+endif
+
+KERNELS = pr pr_spmv
+SUITE = $(KERNELS) converter
+
+.PHONY: all
+all: $(SUITE)
+
+$(SUITE): % : src/%.cc src/*.h
+	$(CXX) $(CXX_FLAGS) $< -o $@
+
+.PHONY: clean 
+clean: 
+	rm -f $(SUITE) test/out/*                                
